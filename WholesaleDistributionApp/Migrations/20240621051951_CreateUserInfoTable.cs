0d37e95a6698e3168adafa4985732bfc56a9515e
Adrian <adrianfwl0329@gmail.com>
2024-06-21 22:47:29 +0800

    Dev: Login, Registration, UserInfo Table, View Account Details, Logout, DIsplay Role & Username

---
+++ WholesaleDistributionApp/Migrations/20240621051951_CreateUserInfoTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WholesaleDistributionApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUserInfoTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserInfo",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserRole = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInfo", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserInfo");
+        }
+    }
+}
---
+++ WholesaleDistributionApp/Models/UserInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WholesaleDistributionApp.Models
+{
+    public class UserInfo
+    {
+        [Key]
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string UserRole { get; set; }
+    }
+}
