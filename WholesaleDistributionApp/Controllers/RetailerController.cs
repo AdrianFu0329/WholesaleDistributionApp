f16b16286c3e17aa0c3093a90b80d21d75016803
peiYueh <peiyueh6633@gmail.com>
2024-07-08 03:13:47 +0800

    manage order status retailer side

--- WholesaleDistributionApp/Controllers/RetailerController.cs
+++ WholesaleDistributionApp/Controllers/RetailerController.cs
@@ -12,10 +12,11 @@ namespace WholesaleDistributionApp.Controllers
     public class RetailerController : Controller
     {
         private readonly WholesaleDistributionAppContext _context;
-
-        public RetailerController(WholesaleDistributionAppContext context)
+        private readonly UserManager<WholesaleDistributionAppUser> _userManager;
+        public RetailerController(WholesaleDistributionAppContext context, UserManager<WholesaleDistributionAppUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
         public IActionResult ViewProducts(string searchString)
         {
@@ -139,6 +140,132 @@ namespace WholesaleDistributionApp.Controllers
             catch (Exception ex)
             {
                 return Json(new { success = false, message = $"Failed to submit order: {ex.Message}" });
+            }
+        }
+
+        public async Task<IActionResult> OrderManagement(string searchString, string category)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var userId = user!.Id;
+
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            // Load Stocks for the current Distributor
+            var orders = _context.Orders
+                                 .Where(s =>
+                                 s.RetailerId == userId)
+                                 .AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                orders = orders.Where(od => od.OrderId.ToString().Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(category) && category != "All")
+            {
+                orders = orders.Where(o => o.OrderStatus == category);
+            }
+
+            return View(orders.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult UpdateOrderStatus(Guid orderId, string status)
+        {
+            try
+            {
+                // Retrieve the order from the database based on orderId
+                var orderToUpdate = _context.Orders.FirstOrDefault(o => o.OrderId == orderId);
+
+                if (orderToUpdate != null)
+                {
+                    /*if (status == "Completed")
+                    {
+                        var stockItems = _context.OrderDetails.Where(od => od.OrderId == orderId).ToList();
+                        foreach (var item in stockItems)
+                        {
+                            var stock = _context.WarehouseStock.FirstOrDefault(s => s.DistributorStockId == item.StockId);
+                            if (status == "Completed")
+                            {
+                                if (stock == null)
+                                {
+                                    //get stock details by stock id
+                                    var newStockDetails = _context.DistributorStock.Find(item.StockId);
+                                    //add new stock
+                                    var newStock = new WarehouseStock
+                                    {
+                                        StockId = Guid.NewGuid().ToString(),
+                                        DistributorStockId = newStockDetails.StockDistributorId, // Need to set here when purchase complete
+                                        ItemName = newStockDetails.ItemName,
+                                        Description = newStockDetails.Description,
+                                        Quantity = item.Quantity,
+                                        SinglePrice = newStockDetails.SinglePrice,
+                                        StockDistributorId = newStockDetails.StockId,
+                                        ImgDownloadURL = newStockDetails.ImgDownloadURL,
+                                        ForRetailerPurchase = false,
+                                    };
+
+                                    _context.WarehouseStock.Add(newStock);
+
+                                }
+                                else
+                                {
+                                    stock.Quantity += item.Quantity;
+                                    _context.WarehouseStock.Update(stock);
+                                }
+                            }
+                        }
+                    }*/
+
+                    // Update the order status
+                    orderToUpdate.OrderStatus = status;
+
+                    // Save changes to the database
+                    _context.SaveChanges();
+
+                    return Json(new { success = true });
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Order not found." });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult SubmitRefundRequest(double refundAmount, string orderId)
+        {
+            try
+            {
+                // Generate new GUID for RefundId
+                var refundId = Guid.NewGuid();
+
+                // Create new RefundRequest object
+                var refundRequest = new RefundRequest
+                {
+                    RefundId = refundId.ToString(),
+                    RequestDate = DateTime.UtcNow,
+                    RefundAmount = refundAmount,
+                    RefundStatus = "Pending",
+                    OrderId = orderId
+                };
+
+                // Add to DbContext and save changes
+                _context.RefundRequest.Add(refundRequest);
+                _context.SaveChanges();
+
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
             }
         }
     }
